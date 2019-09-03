6860cda5d96768220bef5376c4899ad47106e83c
uggi121 <sudharshanmadhavan@outlook.com>
2019-09-03 23:20:50 +0800

    Add task and done status

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Duke {
 
     private static Scanner sc = new Scanner(System.in);
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -19,20 +21,44 @@ public class Duke {
     private static void run() {
         while (checkNextInput()) {
             String input = readInput();
-            if (input.toLowerCase().startsWith("bye")) {
+            if (input.strip().toLowerCase().startsWith("bye")) {
                 displayOutput("Bye! Hope to see you again :-)");
                 break;
-            } else if (input.toLowerCase().startsWith("list")) {
+            } else if (input.strip().toLowerCase().startsWith("list")) {
                 displayOutput(getListOfTasks());
+            } else if (input.strip().toLowerCase().startsWith("done")) {
+                displayOutput(markAsDone(input));
             } else {
                 displayOutput(addTask(input));
             }
         }
     }
 
+    private static String markAsDone(String input) {
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(input);
+        ArrayList<Integer> listOfTaskIds = new ArrayList<>();
+        while (m.find()) {
+            int taskId = Integer.parseInt(m.group());
+            listOfTaskIds.add(taskId);
+        }
+        return getDoneUpdates(listOfTaskIds);
+    }
+
+    private static String getDoneUpdates(ArrayList<Integer> listOfTaskIds) {
+        StringBuilder finalOutput = listOfTaskIds.size() == 1
+                ? new StringBuilder("Nice! I've marked this task as done:\n")
+                : new StringBuilder("Nice! I've marked these tasks as done:\n");
+        for (int id : listOfTaskIds) {
+            tasks.get(id - 1).setDone(true);
+            finalOutput.append("  " + tasks.get(id - 1).toString() + "\n");
+        }
+        return finalOutput.toString();
+    }
+
     private static String addTask(String task) {
-        tasks.add(task);
-        return "added: " + task;
+        tasks.add(new Task(task));
+        return "added: " + tasks.get(tasks.size() - 1).getDescription();
     }
 
     private static String getListOfTasks() {
@@ -43,7 +69,10 @@ public class Duke {
                 output.append("\n");
             }
         }
-        return output.toString();
+        String finalOutput = output.toString();
+        return finalOutput.isBlank()
+                ? "Oops! The list is empty."
+                : finalOutput;
     }
 
     private static String readInput() {
