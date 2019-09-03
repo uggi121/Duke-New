877bcb3fdb6e60a9f1e307b9d876d9caf6a7af48
uggi121 <sudharshanmadhavan@outlook.com>
2019-09-03 22:51:52 +0800

    Add list, storage features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
     private static Scanner sc = new Scanner(System.in);
+    private static ArrayList<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -20,9 +22,28 @@ public class Duke {
             if (input.toLowerCase().startsWith("bye")) {
                 displayOutput("Bye! Hope to see you again :-)");
                 break;
+            } else if (input.toLowerCase().startsWith("list")) {
+                displayOutput(getListOfTasks());
+            } else {
+                displayOutput(addTask(input));
             }
-            displayOutput(input);
         }
+    }
+
+    private static String addTask(String task) {
+        tasks.add(task);
+        return "added: " + task;
+    }
+
+    private static String getListOfTasks() {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < tasks.size(); i++) {
+            output.append((i + 1) + ". " + tasks.get(i));
+            if (i != tasks.size() - 1) {
+                output.append("\n");
+            }
+        }
+        return output.toString();
     }
 
     private static String readInput() {
